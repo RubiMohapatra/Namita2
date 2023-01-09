bf8cc0bafe67f6ea912ef03882a9c917073d3775
Dell <Dell@DESKTOP-G57KNH2>
2023-01-09 17:57:15 +0530

    commit

---
+++ jenkins/src/test/java/jenkins/TestCase3.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase3 {
+	@Test
+	public void m3()
+	{
+		Reporter.log("How r u",true);
+	}
+
+}
