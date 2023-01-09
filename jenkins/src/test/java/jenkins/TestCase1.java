727ea87c75fda655fb5ab3464fe9e15fb3923089
Dell <Dell@DESKTOP-G57KNH2>
2023-01-09 17:02:17 +0530

    jenikins testcases

---
+++ jenkins/src/test/java/jenkins/TestCase1.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Hello World",true);
+	}
+
+}
---
+++ jenkins/src/test/java/jenkins/TestCase2.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("Hello",true);
+	}
+}
+
+	
